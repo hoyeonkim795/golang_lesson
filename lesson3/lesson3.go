d64bdc8303a1e84bedf20235c5fd58c8fd57fae7
Bella_Hoyeon <bella@spoonradio.co>
2021-04-20 18:02:14 +0900

    lesson3 - input text 버그 수정

--- lesson3/lesson3.go
+++ lesson3/lesson3.go
@@ -2,20 +2,25 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	var num int
+
+	var num string
 	for {
 		fmt.Print("숫자를 입력하세요 :")
-		fmt.Scanln(&num)
-		if 0 < num  && num < 101 {
-			for i := 1; i < num+1; i++ {
-				stars := strings.Repeat("*", i)
-				fmt.Println(stars)
+		fmt.Scan(&num)
+		if convertedNum, err := strconv.Atoi(num); err == nil {
+			if convertedNum > 0 && convertedNum < 101 {
+				for i := 1; i < convertedNum+1; i++ {
+					stars := strings.Repeat("*", i)
+					fmt.Println(stars)
+				}
+				break
 			}
-			break
 		}
 	}
+
 }
