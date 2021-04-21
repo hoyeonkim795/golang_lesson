aa9a7de113a415716c91960643816c221d9bf520
Bella_Hoyeon <bella@spoonradio.co>
2021-04-21 18:10:59 +0900

    lesson4 - main.go edit

--- lesson4/main.go
+++ lesson4/main.go
@@ -1,1 +1,26 @@
-package lesson4
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	var num string
+	for {
+		fmt.Print("숫자를 입력하세요 :")
+		fmt.Scan(&num)
+		if convertedNum, err:= strconv.Atoi(num); err == nil{
+			if convertedNum > 0 && convertedNum < 101 {
+				for i := 1; i < convertedNum+1; i++ {
+					stars := strings.Repeat("*", i)
+					fmt.Println(stars)
+				}
+				break
+			}
+		}
+	}
+
+}
