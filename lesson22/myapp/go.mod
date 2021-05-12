191d7f2ac996bc38c6df3454eec3afdb2a46d60f
Bella_Hoyeon <bella@spoonradio.co>
2021-05-12 10:00:23 +0900

    lesson22 create redis-server

---
+++ lesson22/myapp/go.mod
@@ -0,0 +1,8 @@
+module myapp
+
+go 1.16
+
+require (
+	github.com/go-redis/redis/v8 v8.8.2
+	github.com/labstack/echo/v4 v4.3.0
+)
---
+++ lesson22/myapp/redis-server.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"context"
+
+	"github.com/go-redis/redis/v8"
+
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.POST("/save", save)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func save(c echo.Context) error {
+	// Get name and email
+	key := c.FormValue("key")
+	value := c.FormValue("value")
+	ExampleClient(key, value)
+	return c.String(http.StatusOK, "key:" + key + ", value:" + value)
+}
+
+var ctx = context.Background()
+
+func ExampleClient(key string, value string) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	err := rdb.Set(ctx, key, value, 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, key).Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(key, val)
+
+	//val2, err := rdb.Get(ctx, "key2").Result()
+	//if err == redis.Nil {
+	//	fmt.Println("key2 does not exist")
+	//} else if err != nil {
+	//	panic(err)
+	//} else {
+	//	fmt.Println("key2", val2)
+	//}
+	// Output: key value
+	// key2 does not exist
+}
